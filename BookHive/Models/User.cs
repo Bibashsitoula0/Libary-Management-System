285fe680747e32afab7f3a0fda8e6b97d77771c5
Bibash Sitoula <sitoulabibash666@gmail.com>
2023-07-18 22:16:52 +0545

    Added the features of login ,verify otp,regtister

---
+++ BookHive/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace BookHive.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string  Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
